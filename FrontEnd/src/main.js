7b4f30c4a6bef5932e06ee6368bc977324cac6b4
sleepersusu <s900808sw@gmail.com>
2024-12-10 18:11:57 +0800

    1210

--- FrontEnd/src/main.js
+++ FrontEnd/src/main.js
@@ -4,6 +4,7 @@ import axios from 'axios'
 import VueAxios from 'vue-axios'
 import App from './App.vue'
 import router from './router'
+
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import 'bootstrap-icons/font/bootstrap-icons.css'
 import Swal from 'sweetalert2'
@@ -18,5 +19,6 @@ AOS.init()
 app.use(createPinia())
 app.use(router)
 app.use(VueAxios, axios)
+
 
 app.mount('#app')
