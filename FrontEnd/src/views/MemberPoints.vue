84fcda539f3fb4afd64b6bd24f0ff1782402011f
kyle511044 <kyle511044@gmail.com>
2024-12-11 16:16:52 +0800

    簡單測試呈現獎品列表

--- FrontEnd/src/views/MemberPoints.vue
+++ FrontEnd/src/views/MemberPoints.vue
@@ -1,15 +1,39 @@
 <template>
-  <div>MemberPoints</div>
+  <div>
+    <h1>Member Points</h1>
+    <ul>
+      <li v-for="prize in pointPrizes" :key="prize.id">{{ prize.pointPrizesName }}</li>
+    </ul>
+  </div>
 </template>
 
 <script>
 export default {
   data() {
-    return {}
+    return {
+      pointPrizes: [],
+    }
   },
-  methods: {},
+  methods: {
+    async getPointPrizes() {
+      const api = `${import.meta.env.VITE_API}/api/pointPrizes`;
+      try {
+        const response = await fetch(api);
+        if (response.ok) {
+          const data = await response.json();
+          this.pointPrizes = data;
+        } else {
+          console.error('Failed to fetch point prizes:', response.status);
+        }
+      } catch (error) {
+        console.error('Error fetching point prizes:', error);
+      }
+    }
+  },
   computed: {},
   watch: {},
-  created() {},
+  created() {
+    this.getPointPrizes();
+  },
 }
 </script>
