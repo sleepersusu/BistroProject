7151e3690a194e6b3e6c1ff91f613b2a8d496dcb
kyle511044 <kyle511044@gmail.com>
2024-12-12 16:32:06 +0800

    新增兌換商品方法

--- FrontEnd/src/views/MemberPoints.vue
+++ FrontEnd/src/views/MemberPoints.vue
@@ -1,9 +1,25 @@
 <template>
   <div>
-    <h1>Member Points</h1>
-    <ul>
-      <li v-for="prize in pointPrizes" :key="prize.id">{{ prize.pointPrizesName }}</li>
-    </ul>
+    <h1>兌換獎品列表</h1>
+
+<div class="container">
+  <ul class="d-flex flex-wrap">
+    <li v-for="prize in pointPrizes" :key="prize.id" class="d-flex flex-column" style="width: 18rem; margin: 10px;">
+      <div class="card" style="height: 490px; position: relative;">
+        <!-- 動態綁定圖片的 Base64 編碼 -->
+        <img :src="'data:image/jpeg;base64,' + prize.base64Image" class="card-img-top w-100" alt="Prize Image"
+        style="height: 300px; object-fit: cover;">
+        <div class="card-body" style="overflow-y: auto; height: calc(100% - 300px);">
+          <h5 class="card-title">{{ prize.pointPrizesName }}</h5>
+          <p class="card-text">{{ prize.pointPrizesDescription }}</p>
+          <a href="#" class="btn btn-primary mt-auto" style="position: absolute; bottom: 16px;" @click="redeemPrize(prize)">兌換商品</a>
+        </div>
+      </div>
+    </li>
+  </ul>
+</div>
+
+
   </div>
 </template>
 
@@ -28,7 +44,51 @@ export default {
       } catch (error) {
         console.error('Error fetching point prizes:', error);
       }
+    },
+
+    async redeemPrize(prize) {
+  const memberId = 12;  // 假資料，會員 ID 為 9
+  const pointPrizesId = prize.id;  // 商品 ID 來自按下的商品
+  const recordsDate = new Date().toISOString();  // 使用當前時間作為兌換日期
+
+  const requestData = {
+    memberId,
+    pointPrizesId,
+    recordsDate,
+  };
+
+  // 顯示兌換成功的彈窗
+  window.Swal.fire({
+    toast: true,
+    position: 'top-end',
+    icon: 'success',
+    title: `兌換成功！您已成功兌換 ${prize.pointPrizesName}`,
+    timer: 1500,
+    showConfirmButton: false,
+    timerProgressBar: true,
+  });
+
+  try {
+    const api = `${import.meta.env.VITE_API}/api/pointRecord`;
+    const response = await fetch(api, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(requestData),  // 將資料轉換為 JSON 格式發送
+    });
+
+    if (response.ok) {
+      console.log('兌換紀錄成功');
+    } else {
+      console.error('Failed to record point:', response.status);
     }
+  } catch (error) {
+    console.error('Error sending request:', error);
+  }
+}
+
+
   },
   computed: {},
   watch: {},
@@ -37,3 +97,9 @@ export default {
   },
 }
 </script>
+
+<style>
+.card-body::-webkit-scrollbar {
+  display: none;
+}
+</style>
