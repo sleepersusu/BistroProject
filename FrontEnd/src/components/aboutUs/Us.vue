19db9bf3f5bebe6933681799eb45a01c2b8b328e
sleepersusu <s900808sw@gmail.com>
2024-12-14 20:30:09 +0800

    1214 finished about us

---
+++ FrontEnd/src/components/aboutUs/Us.vue
@@ -0,0 +1,19 @@
+<template>
+
+</template>
+
+<script>
+
+export default {
+  name:"Us",
+  data(){
+    return{
+    }
+  }
+}
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ FrontEnd/src/components/cart/CartTitle.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name:"CartTitle",
+  data(){
+    return{
+    }
+  }
+}
+</script>
