6a5fe240a5305fffcdff9624752d6183c38a702d
Feather <hong1011227@gmail.com>
2024-12-18 16:39:39 +0800

    增加跳出成功通知，修改部分bug

--- FrontEnd/src/main.js
+++ FrontEnd/src/main.js
@@ -9,15 +9,23 @@ import router from './router'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import 'bootstrap-icons/font/bootstrap-icons.css'
 import Swal from 'sweetalert2'
+import 'sweetalert2/dist/sweetalert2.min.css';
 import 'aos/dist/aos.css'
 import AOS from 'aos'
 import { Form, Field, ErrorMessage, defineRule, configure } from 'vee-validate'
 import { required, email, min, max, regex } from '@vee-validate/rules'
 import { localize, setLocale } from '@vee-validate/i18n'
 import zhTW from '@vee-validate/i18n/dist/locale/zh_TW.json'
-
 import VueLuckyCanvas from '@lucky-canvas/vue'
 import StarRating from 'vue-star-rating'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { faEnvelope } from '@fortawesome/free-regular-svg-icons';
+
+library.add(faUserSecret,fas,faEnvelope)
+
 
 defineRule('required', required)
 defineRule('email', email)
@@ -48,5 +56,5 @@ app.component('Loading', Loading)
 app.component('Form', Form)
 app.component('Field', Field)
 app.component('ErrorMessage', ErrorMessage)
-
+app.component('font-awesome-icon', FontAwesomeIcon)
 app.mount('#app')
--- FrontEnd/src/views/Reservations.vue
+++ FrontEnd/src/views/Reservations.vue
@@ -1,75 +1,53 @@
 <template>
-  <div
-    class="container-fluid py-4"
-    style="
+  <div class="container-fluid py-4" style="
       background-image: url('images/reservation-bg.jpg');
       color: white;
       padding-left: 50px;
       padding-right: 50px;
-    "
-  >
+    ">
     <div class="row g-4 align-items-center justify-content-center">
-      <div
-        class="col-lg-5 col-md-12"
-        style="
+      <div class="col-lg-5 col-md-12" style="
           display: flex;
           flex-direction: column;
           justify-content: center;
           margin-left: 50px;
           margin-right: 25px;
-        "
-      >
+          width: 30%;
+        ">
         <div id="googlemap" class="mb-4" style="width: 100%; height: 350px">
           <iframe
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3671.9583749490967!2d120.22380107477024!3d23.025300516227354!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x346e771f2995cba5%3A0x3bfd449f1e46ffef!2z5Y2X6Ie656eR5oqA5aSn5a24!5e0!3m2!1szh-TW!2stw!4v1734080088780!5m2!1szh-TW!2stw"
-            class="w-100 h-100 border rounded"
-            allowfullscreen=""
-            loading="lazy"
-            referrerpolicy="no-referrer-when-downgrade"
-            style="border-radius: 10px"
-          ></iframe>
+            class="w-100 h-100 border rounded" allowfullscreen="" loading="lazy"
+            referrerpolicy="no-referrer-when-downgrade" style="border-radius: 10px"></iframe>
         </div>
-        <div
-          id="inform"
-          class="col-12"
-          style="background-color: transparent; padding: 20px; border-radius: 10px"
-        >
+        <div id="inform" class="col-12" style="background-color: transparent; padding: 20px; ">
           <div class="row">
-            <div class="col-lg-6 mb-3">
-              <div class="phone">
-                <i class="fa fa-phone fs-4 me-3"></i>
+            <div class="col-lg-5 mb-3" style="background-color: white;margin: 20px;border-radius: 3px">
+              <div class="phone text-black" style="border-color: aliceblue;">
+                <font-awesome-icon :icon="['fas', 'phone']" />
                 <h5 class="fw-bold">Phone Numbers</h5>
                 <span>
-                  <a href="tel:07-656-8106" class="text-decoration-none text-light d-block"
-                    >07-656-8106</a
-                  >
-                  <a href="tel:07-656-8107" class="text-decoration-none text-light d-block"
-                    >07-656-8107</a
-                  >
+                  <p>07-656-8106</p>
+                  <p>07-716-9502</p>
                 </span>
               </div>
             </div>
-            <div class="col-lg-6 mb-3">
-              <div class="message">
-                <i class="fa fa-envelope fs-4 me-3"></i>
+            <div class="col-lg-5 mb-3" style="background-color: white;margin: 20px;border-radius: 3px">
+              <div class="message" style="color: black;">
+
+                <font-awesome-icon :icon="['far', 'envelope']" />
                 <h5 class="fw-bold">Emails</h5>
                 <span>
-                  <a href="mailto:hello@company.com" class="text-decoration-none text-light d-block"
-                    >hello@company.com</a
-                  >
-                  <a href="mailto:info@company.com" class="text-decoration-none text-light d-block"
-                    >info@company.com</a
-                  >
+                  <p>@company.company</p>
+                  <p>info@company.com</p>
                 </span>
               </div>
             </div>
           </div>
         </div>
       </div>
-      <div
-        id="app"
-        class="col-lg-5 col-md-12"
-        style="
+
+      <div id="app" class="col-lg-5 col-md-12" style="
           background-color: white;
           padding: 20px;
           border-radius: 10px;
@@ -77,70 +55,42 @@
           color: black;
           margin-left: 25px;
           margin-right: 50px;
-        "
-      >
+          width: 30%;
+        ">
         <div class="mb-4 text-center">
           <h4 class="fw-bold">訂位資訊</h4>
         </div>
         <form @submit.prevent="submitReservation">
           <div class="mb-3">
             <label for="customerName" class="form-label">訂位人姓名</label>
-            <input
-              type="text"
-              class="form-control frame"
-              id="customerName"
-              v-model="reservations.customerName"
-              required
-            />
+
+            <input type="text" class="form-control frame" id="customerName" v-model="reservations.customerName"
+              required />
           </div>
           <div class="mb-3">
             <label class="form-label">性別</label>
             <div class="d-flex">
               <div class="form-check me-3">
-                <input
-                  type="radio"
-                  class="form-check-input"
-                  name="customerGender"
-                  v-model="reservations.customerGender"
-                  value="男"
-                  id="genderMale"
-                  required
-                />
+                <input type="radio" class="form-check-input" name="customerGender" v-model="reservations.customerGender"
+                  value="男" id="genderMale" required />
                 <label class="form-check-label" for="genderMale">男</label>
               </div>
               <div class="form-check">
-                <input
-                  type="radio"
-                  class="form-check-input"
-                  name="customerGender"
-                  v-model="reservations.customerGender"
-                  value="女"
-                  id="genderFemale"
-                  required
-                />
+                <input type="radio" class="form-check-input" name="customerGender" v-model="reservations.customerGender"
+                  value="女" id="genderFemale" required />
                 <label class="form-check-label" for="genderFemale">女</label>
               </div>
             </div>
           </div>
           <div class="mb-3">
             <label for="contactPhone" class="form-label">電話</label>
-            <input
-              type="tel"
-              class="form-control frame"
-              id="contactPhone"
-              v-model="reservations.contactPhone"
-              required
-            />
+            <input type="tel" class="form-control frame" id="contactPhone" v-model="reservations.contactPhone"
+              required />
           </div>
           <div class="row g-3">
             <div class="col-md-6">
               <label for="numberPeople" class="form-label">訂位人數</label>
-              <select
-                class="form-select frame"
-                id="numberPeople"
-                v-model.number="reservations.numberPeople"
-                required
-              >
+              <select class="form-select frame" id="numberPeople" v-model.number="reservations.numberPeople" required>
                 <option value="0" selected>選擇人數</option>
                 <option value="1">1位</option>
                 <option value="2">2位</option>
@@ -150,39 +100,25 @@
             </div>
             <div class="col-md-6">
               <label for="reservationDate" class="form-label">日期</label>
-              <input
-                type="date"
-                class="form-control frame"
-                id="reservationDate"
-                v-model="reservations.reservationDate"
-                v-bind:min="minDate"
-                v-bind:max="maxDate"
-                required
-              />
+              <input type="date" class="form-control frame" id="reservationDate" v-model="reservations.reservationDate"
+                v-bind:min="minDate" v-bind:max="maxDate" required />
             </div>
           </div>
           <div class="mb-4 mt-3">
             <label class="form-label">時段</label>
             <div>
-              <button
-                v-for="time in availableTimeslots"
-                :key="time"
-                class="btn btn-outline-success w-100 me-2 mb-2"
-                @click="handleClick(time, $event)"
-              >
+              <button v-for="time in availableTimeslots" :key="time" :class="{
+                'btn-outline-success': selectedTime !== time,
+                'btn-success': selectedTime === time,
+              }" class="btn btn-outline-success w-100 me-2 mb-2" @click="handleClick(time, $event)">
                 {{ time }}
               </button>
             </div>
-            <input type="hidden" v-model="reservations.startTime" />
+            <input type="text" v-model="reservations.startTime" />
           </div>
           <div class="mb-3">
             <label for="notes" class="form-label">備註</label>
-            <textarea
-              class="form-control frame"
-              id="notes"
-              rows="4"
-              v-model="reservations.notes"
-            ></textarea>
+            <textarea class="form-control frame" id="notes" rows="4" v-model="reservations.notes"></textarea>
           </div>
           <div class="text-center">
             <button type="submit" class="btn btn-success w-100">確認訂位</button>
@@ -194,7 +130,7 @@
 </template>
 
 <script>
-//記得要修改成響應式
+
 export default {
   data() {
     return {
@@ -210,13 +146,25 @@ export default {
       minDate: '',
       maxDate: '',
       availableTimeslots: [],
+      selectedTime: '',
     }
   },
   methods: {
+    showSuccess() {
+      Swal.fire({
+        title: '新增訂位成功!',
+        icon: 'success',
+        confirmButtonText: '確定',
+      });
+    },
     handleClick(time, event) {
       event.preventDefault() // 阻止按鈕的默認行為，防止它觸發表單提交
-      console.log('按鈕點擊時間:', time)
-      this.reservations.startTime = time
+      if (this.selectedTime === time) {
+        this.selectedTime = '';
+      } else {
+        this.selectedTime = time;
+      }
+      this.reservations.startTime = this.selectedTime;
     },
     formatDate(date) {
       if (!date) return ''
@@ -245,7 +193,7 @@ export default {
           this.availableTimeslots = []
         }
       } catch (e) {
-        console.error('這是錯的:', e)
+        console.error('錯誤:', e)
         this.availableTimeslots = []
       }
     },
@@ -269,7 +217,7 @@ export default {
         const api = `${import.meta.env.VITE_API}/api/Bistro/insert`
         const response = await this.axios.post(api, this.reservations)
         if (response.data.success) {
-          alert('訂位成功！')
+          this.showSuccess();
           this.reservations = {
             customerName: '',
             customerGender: '',
@@ -279,6 +227,7 @@ export default {
             numberPeople: 0,
             notes: '',
           }
+
         } else {
           const errorMessage = response.data.message || '訂位失敗，請稍後再試。'
           alert(`錯誤: ${errorMessage}`)
@@ -286,7 +235,7 @@ export default {
       } catch (error) {
         if (error.response && error.response.data) {
           const errorMessage = error.response.data.message || '提交訂位資料時發生錯誤，請稍後再試。'
-          alert(`錯誤: ${errorMessage}`)
+          alert(`格式輸入錯誤: ${errorMessage}`)
         } else {
           console.error('錯誤:', error)
           alert('提交訂位資料時發生錯誤，請稍後再試。')
@@ -352,8 +301,7 @@ form {
   color: black;
 }
 
-.form-label,
-.form-control {
+.text-black {
   color: black;
 }
 </style>
