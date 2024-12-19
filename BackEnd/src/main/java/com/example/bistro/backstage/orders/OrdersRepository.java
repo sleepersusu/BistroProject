bbfb0cbb61cc37ec686b7896ad19cbc55519933d
sleepersusu <s900808sw@gmail.com>
2024-12-19 17:13:41 +0800

    1219 memberCart ok

--- BackEnd/src/main/java/com/example/bistro/backstage/orders/OrdersRepository.java
+++ BackEnd/src/main/java/com/example/bistro/backstage/orders/OrdersRepository.java
@@ -8,4 +8,5 @@ public interface OrdersRepository extends JpaRepository<Orders, Integer> {
 
     Optional<Orders> findByOrdersTel(String ordersTel);  // 根據電話查詢訂單
 
+    Optional<Orders> findByOrdersNumber(String ordersNumber);
 }
