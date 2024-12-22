158eeacdbe5c3fbf42d80f7cafa37e9d5700ac13
Feather <hong1011227@gmail.com>
2024-12-22 23:55:11 +0800

    API簡訊串接，修改人數上限，大改後端controller，service

--- BackEnd/src/main/java/com/example/bistro/frontstage/reservations/ReservationRestController.java
+++ BackEnd/src/main/java/com/example/bistro/frontstage/reservations/ReservationRestController.java
@@ -1,8 +1,10 @@
 package com.example.bistro.frontstage.reservations;
 
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,17 +12,19 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.bistro.backstage.reservations.Reservations;
+
 @RestController
 public class ReservationRestController {
 
 	@Autowired
-	private ReservationsFrontstageService reservationsFrontstageService;
+	private ReservationsFrontstageService testService;
 
 	@PostMapping("/api/Bistro/insert")
 	public ResponseEntity<Map<String, Object>> insert(@RequestBody ReservationDTO dto) {
 
 		try {
-			reservationsFrontstageService.insert(dto);
+			testService.insert(dto);
 			Map<String, Object> response = new HashMap<>();
 			response.put("success", true);
 			response.put("message", "訂位成功");
@@ -41,8 +45,8 @@ public class ReservationRestController {
 	@PostMapping("/api/Bistro/remaining")
 	public ResponseEntity<Map<String, Object>> remaining(@RequestBody ReservationDTO reservation) {
 		try {
-			List<String> availableTimeslots = reservationsFrontstageService
-					.getAvailableTimeSlots(reservation.getReservationDate(), reservation.getNumberPeople());
+			List<String> availableTimeslots = testService.getAvailableTimeSlots(reservation.getReservationDate(),
+					reservation.getNumberPeople());
 
 			if (!availableTimeslots.isEmpty()) {
 				Map<String, Object> response = new HashMap<>();
@@ -56,6 +60,29 @@ public class ReservationRestController {
 			Map<String, Object> response = new HashMap<>();
 			response.put("success", false);
 			response.put("message", "查詢可用時間段時發生錯誤，請稍後再試。");
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
+		}
+	}
+
+	// 接API簡訊功能
+	@PostMapping("/api/Bistro/send")
+	public ResponseEntity<Map<String, Object>> submitReservation(@RequestBody ReservationDTO dto) {
+		try {
+			String contactPhone = dto.getContactPhone();
+			Date reservationDate = dto.getReservationDate();
+			String startTime = dto.getStartTime();
+			Integer numberPeople = dto.getNumberPeople();
+
+			testService.sendReservationConfirmation(contactPhone, reservationDate, startTime, numberPeople);
+
+			Map<String, Object> response = new HashMap<>();
+			response.put("success", true);
+			response.put("message", "訂位成功，簡訊送出");
+			return ResponseEntity.ok(response);
+		} catch (Exception e) {
+			Map<String, Object> response = new HashMap<>();
+			response.put("success", false);
+			response.put("message", "訂位簡訊發送失敗");
 			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
 		}
 	}
--- FrontEnd/src/views/Reservations.vue
+++ FrontEnd/src/views/Reservations.vue
@@ -1,21 +1,24 @@
 <template>
-  <div class="container-fluid py-4" style="
+  <div
+    class="container-fluid py-4"
+    style="
       background-image: url('images/reservation-bg.jpg');
       color: white;
       padding-left: 50px;
       padding-right: 50px;
-    ">
+    "
+  >
     <div class="row g-4 justify-content-center d-flex align-items-center" style="min-height: 100vh">
       <div class="col-lg-7 col-md-12">
         <div id="googlemap" class="mb-4" style="width: 100%; height: 350px">
           <iframe
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3671.9583749490967!2d120.22380107477024!3d23.025300516227354!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x346e771f2995cba5%3A0x3bfd449f1e46ffef!2z5Y2X6Ie656eR5oqA5aSn5a24!5e0!3m2!1szh-TW!2stw!4v1734080088780!5m2!1szh-TW!2stw"
-            class="w-100 h-100" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" style="
-              max-width: calc(100% - 200px);
-              margin-left: auto;
-              margin-right: auto;
-              display: block;
-            "></iframe>
+            v-if="isMapVisible"
+            class="w-100 h-100"
+            allowfullscreen=""
+            referrerpolicy="no-referrer-when-downgrade"
+            style="width: 100%; height: 100%"
+          ></iframe>
         </div>
         <div id="inform" class="container py-4" style="background-color: transparent">
           <div class="row g-4 justify-content-center">
@@ -23,8 +26,12 @@
               <div class="card text-center p-4 position-relative">
                 <div
                   class="position-absolute top-0 start-50 translate-middle bg-black text-white rounded-circle d-flex justify-content-center align-items-center"
-                  style="width: 60px; height: 60px; z-index: 10">
-                  <font-awesome-icon :icon="['fas', 'phone']" style="font-size: 27px"></font-awesome-icon>
+                  style="width: 60px; height: 60px; z-index: 10"
+                >
+                  <font-awesome-icon
+                    :icon="['fas', 'phone']"
+                    style="font-size: 27px"
+                  ></font-awesome-icon>
                 </div>
                 <div class="mt-3">
                   <h5 class="fw-bold">Phone Numbers</h5>
@@ -37,12 +44,16 @@
               <div class="card text-center p-4 position-relative">
                 <div
                   class="position-absolute top-0 start-50 translate-middle bg-black text-white rounded-circle d-flex justify-content-center align-items-center"
-                  style="width: 60px; height: 60px; z-index: 10">
-                  <font-awesome-icon :icon="['far', 'envelope']" style="font-size: 27px"></font-awesome-icon>
+                  style="width: 60px; height: 60px; z-index: 10"
+                >
+                  <font-awesome-icon
+                    :icon="['far', 'envelope']"
+                    style="font-size: 27px"
+                  ></font-awesome-icon>
                 </div>
                 <div class="mt-3">
                   <h5 class="fw-bold">Emails</h5>
-                  <p>rest557@gmail.com</p>
+                  <p>NightSips1223@gmail.com</p>
                   <p>aiz777vic@gmail.com</p>
                 </div>
               </div>
@@ -50,46 +61,79 @@
           </div>
         </div>
       </div>
-      <div id="app" class="col-lg-4 col-md-12" style="
+      <div
+        id="app"
+        class="col-lg-4 col-md-12"
+        style="
           background-color: white;
           padding: 20px;
           border-radius: 10px;
           box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
           color: black;
-        ">
+        "
+      >
         <div class="mb-4 text-center">
           <h4 class="fw-bold">訂位資訊</h4>
         </div>
         <form @submit.prevent="submitReservation">
           <div class="mb-3">
             <label for="customerName" class="form-label">訂位人姓名</label>
-            <input type="text" class="form-control frame" id="customerName" v-model="reservations.customerName"
-              required />
+            <input
+              type="text"
+              class="form-control frame"
+              id="customerName"
+              v-model="reservations.customerName"
+              required
+            />
           </div>
           <div class="mb-3">
             <label class="form-label">性別</label>
             <div class="d-flex">
               <div class="form-check me-3">
-                <input type="radio" class="form-check-input" name="customerGender" v-model="reservations.customerGender"
-                  value="男" id="genderMale" required />
+                <input
+                  type="radio"
+                  class="form-check-input"
+                  name="customerGender"
+                  v-model="reservations.customerGender"
+                  value="男"
+                  id="genderMale"
+                  required
+                />
                 <label class="form-check-label" for="genderMale">男</label>
               </div>
               <div class="form-check">
-                <input type="radio" class="form-check-input" name="customerGender" v-model="reservations.customerGender"
-                  value="女" id="genderFemale" required />
+                <input
+                  type="radio"
+                  class="form-check-input"
+                  name="customerGender"
+                  v-model="reservations.customerGender"
+                  value="女"
+                  id="genderFemale"
+                  required
+                />
                 <label class="form-check-label" for="genderFemale">女</label>
               </div>
             </div>
           </div>
           <div class="mb-3">
             <label for="contactPhone" class="form-label">電話</label>
-            <input type="tel" class="form-control frame" id="contactPhone" v-model="reservations.contactPhone"
-              required />
+            <input
+              type="tel"
+              class="form-control frame"
+              id="contactPhone"
+              v-model="reservations.contactPhone"
+              required
+            />
           </div>
           <div class="row g-3">
             <div class="col-md-6">
               <label for="numberPeople" class="form-label">訂位人數</label>
-              <select class="form-select frame" id="numberPeople" v-model.number="reservations.numberPeople" required>
+              <select
+                class="form-select frame"
+                id="numberPeople"
+                v-model.number="reservations.numberPeople"
+                required
+              >
                 <option value="0" selected>選擇人數</option>
                 <option value="1">1位</option>
                 <option value="2">2位</option>
@@ -103,18 +147,31 @@
             </div>
             <div class="col-md-6">
               <label for="reservationDate" class="form-label">日期</label>
-              <input type="date" class="form-control frame" id="reservationDate" v-model="reservations.reservationDate"
-                v-bind:min="minDate" v-bind:max="maxDate" required />
+              <input
+                type="date"
+                class="form-control frame"
+                id="reservationDate"
+                v-model="reservations.reservationDate"
+                v-bind:min="minDate"
+                v-bind:max="maxDate"
+                required
+              />
             </div>
           </div>
           <div class="mb-4 mt-3">
             <label class="form-label">時段</label>
             <div>
-              <button v-for="time in availableTimeslots" :key="time" :class="{
-                'btn-outline-success': selectedTime !== time,
-                'btn-success': selectedTime === time,
-              }" :style="{ color: selectedTime === time ? '#fff' : '' }"
-                class="btn btn-outline-success w-100 me-2 mb-2" @click="handleClick(time, $event)">
+              <button
+                v-for="time in availableTimeslots"
+                :key="time"
+                :class="{
+                  'btn-outline-black': selectedTime === time,
+                  'btn-black': selectedTime !== time,
+                }"
+                :style="{ color: selectedTime === time ? '#fff' : '' }"
+                class="btn btn-black w-100 me-2 mb-2"
+                @click="handleClick(time, $event)"
+              >
                 {{ time }}
               </button>
             </div>
@@ -122,7 +179,12 @@
           </div>
           <div class="mb-3">
             <label for="notes" class="form-label">備註</label>
-            <textarea class="form-control frame" id="notes" rows="4" v-model="reservations.notes"></textarea>
+            <textarea
+              class="form-control frame"
+              id="notes"
+              rows="4"
+              v-model="reservations.notes"
+            ></textarea>
           </div>
           <div class="text-center">
             <button type="submit" class="btn btn-black w-100">確認訂位</button>
@@ -137,6 +199,7 @@
 export default {
   data() {
     return {
+      isMapVisible: false, 
       reservations: {
         customerName: '',
         customerGender: '',
@@ -158,6 +221,10 @@ export default {
         title: '新增訂位成功!',
         icon: 'success',
         confirmButtonText: '確定',
+        confirmButtonColor: '#000000',
+        customClass: {
+        confirmButton: 'custom-button' 
+    }
       })
     },
     handleClick(time, event) {
@@ -215,20 +282,34 @@ export default {
         alert('請填寫所有必填欄位！')
         return
       }
-
       try {
         const api = `${import.meta.env.VITE_API}/api/Bistro/insert`
         const response = await this.axios.post(api, this.reservations)
         if (response.data.success) {
-          this.showSuccess()
-          this.reservations = {
-            customerName: '',
-            customerGender: '',
-            contactPhone: '',
-            reservationDate: new Date().toISOString().split('T')[0],
-            startTime: '',
-            numberPeople: 0,
-            notes: '',
+          // 發送簡訊要取得的資料
+          const confirmationApi = `${import.meta.env.VITE_API}/api/Bistro/send`
+          const reservationData = {
+            contactPhone: this.reservations.contactPhone,
+            reservationDate: this.reservations.reservationDate,
+            startTime: this.reservations.startTime,
+            numberPeople: this.reservations.numberPeople,
+          }
+          const smsResponse = await this.axios.post(confirmationApi, reservationData)
+          if (smsResponse.data.success) {
+            this.showSuccess()
+            this.reservations = {
+              customerName: '',
+              customerGender: '',
+              contactPhone: '',
+              reservationDate: new Date().toISOString().split('T')[0],
+              startTime: '',
+              numberPeople: 0,
+              notes: '',
+            }
+            this.selectedTime = ''
+          } else {
+            const errorMessage = smsResponse.data.message || '訂位確認簡訊發送失敗，請稍後再試。'
+            alert(`錯誤: ${errorMessage}`)
           }
         } else {
           const errorMessage = response.data.message || '訂位失敗，請稍後再試。'
@@ -245,13 +326,19 @@ export default {
       }
     },
   },
-  mounted() {
+  mounted()
+   {
+    this.$nextTick(() => {
+      this.isMapVisible = true; 
+    });
     const today = new Date()
     this.minDate = today.toISOString().split('T')[0]
     const max = new Date()
     max.setDate(today.getDate() + 7)
     this.maxDate = max.toISOString().split('T')[0]
+    
   },
+  
   computed: {},
   watch: {
     'reservations.numberPeople': function () {
@@ -268,17 +355,42 @@ export default {
 </script>
 
 <style scoped>
+
 .btn-black {
   background-color: black;
   color: white;
   border: 1px solid black;
 }
 
+.btn-outline-black{
+  background-color: darkgray;
+  color: black;
+  
+}
+
 .btn-black:hover {
   background-color: darkgray;
   color: white;
 }
+.custom-button {
+  color: white;  
+}
 
+@media (min-width: 992px) {
+  #googlemap iframe {
+    max-width: calc(100% - 250px);
+    margin-left: auto;
+    margin-right: auto;
+    display: block;
+  }
+}
+
+/* 手機版樣式 */
+@media (max-width: 991px) {
+  #googlemap iframe {
+    max-width: 100%;
+  }
+}
 /* .btn-orange {
   background-color: #ff6600;
   border: 1px solid #ff6600;
@@ -313,7 +425,7 @@ export default {
 } */
 
 /*:class="{
-          'btn-outline-orange': selectedTime !== time,  
+          'btn-outline-orange': selectedTime !== time,
           'btn-orange': selectedTime === time,  改顏色要加入這段
         }"  */
 </style>
